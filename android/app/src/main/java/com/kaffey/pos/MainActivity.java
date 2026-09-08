62cb44d6ad2500811c247d68ead6c11b502c669d
benjligeralde2 <benjligeralde2@gmail.com>
2026-09-08 23:26:38 +0800

    changes orientation base on device

--- android/app/src/main/java/com/kaffey/pos/MainActivity.java
+++ android/app/src/main/java/com/kaffey/pos/MainActivity.java
@@ -1,5 +1,22 @@
 package com.kaffey.pos;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		lockOrientation();
+		super.onCreate(savedInstanceState);
+	}
+
+	private void lockOrientation() {
+		boolean isTablet = getResources().getBoolean(R.bool.is_tablet);
+		setRequestedOrientation(
+			isTablet
+				? ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE
+				: ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+		);
+	}
+}
